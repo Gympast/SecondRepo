082cc27e70768900496402ae1005ede0b24bf66e
Gympast <112376007+Gympast@users.noreply.github.com>
2023-08-29 10:35:56 +0200

    initial

---
+++ SecondRepo/SecondRepo/Program.cs
@@ -0,0 +1,29 @@
+﻿
+//övning 1
+
+string name = Console.ReadLine();
+
+Console.WriteLine($"Hello {name}!");
+
+///////////////////////////////////////////////
+
+//övning 2
+string passWord = "Hoppsan";
+
+if (Console.ReadLine() == passWord)
+{
+    Console.WriteLine("Rätt");
+}
+else
+{
+    Console.WriteLine("FEL FEL FEL FEL FEL");
+}
+///////////////////////////////////////////////
+
+//övning 3
+
+Console.Write("Write a number:");
+
+string nmrTry = Console.ReadLine();
+
+int nmr = int.TryParse(nmrTry);
