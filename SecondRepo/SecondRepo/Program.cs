4266a5b88ad74f4a830011fbe8fdaeda9429a9a3
Gympast <112376007+Gympast@users.noreply.github.com>
2023-08-29 11:26:51 +0200

    övning 4

--- SecondRepo/SecondRepo/Program.cs
+++ SecondRepo/SecondRepo/Program.cs
@@ -1,6 +1,6 @@
 ﻿
 //övning 1
-
+Console.Write("Write your name: ");
 string name = Console.ReadLine();
 
 Console.WriteLine($"Hello {name}!");
@@ -8,6 +8,8 @@ Console.WriteLine($"Hello {name}!");
 ///////////////////////////////////////////////
 
 //övning 2
+
+Console.Write("Enter your password: ");
 string passWord = "Hoppsan";
 
 if (Console.ReadLine() == passWord)
@@ -24,6 +26,60 @@ else
 
 Console.Write("Write a number:");
 
-string nmrTry = Console.ReadLine();
 
-int nmr = int.TryParse(nmrTry);
+
+while (true)
+{
+    string nmrTry = Console.ReadLine();
+    int nmr = 0;
+    bool nmrBool = int.TryParse(nmrTry, out nmr);
+    if (nmrBool == true)
+    {
+        if (nmr == 666)
+        {
+            Console.WriteLine("The program died...");
+            break;
+        }
+        if (nmr < 100)
+        {
+            Console.WriteLine("Smaller than 100");
+            Console.Write("Write another number: ");
+            Console.WriteLine("Or to continue type 666");
+        }
+        else if (nmr > 100)
+        {
+            Console.WriteLine("Bigger than 100");
+            Console.Write("Write another number: ");
+            Console.WriteLine("Or to continue type 666");
+        }
+        else
+        {
+            Console.WriteLine("Number is 100");
+            Console.Write("Write another number: ");
+            Console.WriteLine("Or to continue type 666");
+        }
+    }
+    else
+    {
+        Console.WriteLine("Write a number... dickhead...");
+    }
+}
+
+/////////////////////////////////////////////////////////////
+
+
+//övning 4
+Console.Write("Write your name: ");
+name = Console.ReadLine();
+
+Console.Write("How many times do you want to repeat it? Default is one");
+
+string nmrTimesString = Console.ReadLine();
+
+int nmrTimes = 1;
+bool nmrTimesBool = int.TryParse(nmrTimesString, out nmrTimes);
+
+for (int i = 0; i < nmrTimes; i++)
+{
+    Console.WriteLine($"Hello {name}!");
+}
