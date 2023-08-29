defc5d5a293263ad2a9a122c426a3ec983de6f7e
Gympast <112376007+Gympast@users.noreply.github.com>
2023-08-29 21:10:14 +0200

    init

--- SecondRepo/SecondRepo/Program.cs
+++ SecondRepo/SecondRepo/Program.cs
@@ -6,7 +6,6 @@ static void ovning1()
 
     Console.WriteLine($"Hello {name}!");
 }
-
 static void ovning2()
 {
     Console.Write("Enter your password: ");
@@ -21,66 +20,63 @@ static void ovning2()
         Console.WriteLine("FEL FEL FEL FEL FEL");
     }
 }
-///////////////////////////////////////////////
-
-//övning 3
-
-Console.Write("Write a number:");
-
-
-
-while (true)
+static void ovning3()
 {
-    string nmrTry = Console.ReadLine();
-    int nmr = 0;
-    bool nmrBool = int.TryParse(nmrTry, out nmr);
-    if (nmrBool == true)
+
+    Console.Write("Write a number:");
+
+    while (true)
     {
-        if (nmr == 666)
+        string nmrTry = Console.ReadLine();
+        int nmr = 0;
+        bool nmrBool = int.TryParse(nmrTry, out nmr);
+        if (nmrBool == true)
         {
-            Console.WriteLine("The program died...");
-            break;
-        }
-        if (nmr < 100)
-        {
-            Console.WriteLine("Smaller than 100");
-            Console.Write("Write another number: ");
-            Console.WriteLine("Or to continue type 666");
-        }
-        else if (nmr > 100)
-        {
-            Console.WriteLine("Bigger than 100");
-            Console.Write("Write another number: ");
-            Console.WriteLine("Or to continue type 666");
+            if (nmr == 666)
+            {
+                Console.WriteLine("The program died...");
+                break;
+            }
+
+            if (nmr < 100)
+            {
+                Console.WriteLine("Smaller than 100");
+                Console.Write("Write another number: ");
+                Console.WriteLine("Or to continue type 666");
+            }
+            else if (nmr > 100)
+            {
+                Console.WriteLine("Bigger than 100");
+                Console.Write("Write another number: ");
+                Console.WriteLine("Or to continue type 666");
+            }
+            else
+            {
+                Console.WriteLine("Number is 100");
+                Console.Write("Write another number: ");
+                Console.WriteLine("Or to continue type 666");
+            }
         }
         else
         {
-            Console.WriteLine("Number is 100");
-            Console.Write("Write another number: ");
-            Console.WriteLine("Or to continue type 666");
+            Console.WriteLine("Write a number... dickhead...");
         }
     }
-    else
-    {
-        Console.WriteLine("Write a number... dickhead...");
-    }
 }
-
-/////////////////////////////////////////////////////////////
-
-
-//övning 4
-Console.Write("Write your name: ");
-name = Console.ReadLine();
-
-Console.Write("How many times do you want to repeat it? Default is one");
-
-string nmrTimesString = Console.ReadLine();
-
-int nmrTimes = 1;
-bool nmrTimesBool = int.TryParse(nmrTimesString, out nmrTimes);
-
-for (int i = 0; i < nmrTimes; i++)
+static void ovning4()
 {
-    Console.WriteLine($"Hello {name}!");
+    Console.Write("Write your name: ");
+    name = Console.ReadLine();
+
+    Console.Write("How many times do you want to repeat it? Default is one");
+
+    string nmrTimesString = Console.ReadLine();
+
+    int nmrTimes = 1;
+    bool nmrTimesBool = int.TryParse(nmrTimesString, out nmrTimes);
+
+    for (int i = 0; i < nmrTimes; i++)
+    {
+        Console.WriteLine($"Hello {name}!");
+    }
 }
